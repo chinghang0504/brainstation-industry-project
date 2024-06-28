f1a29e9366ce61fc01dcb6395b903e488ad37820
Ching Hang Lam <chinghang0504@gmail.com>
2024-06-28 01:50:23 -0700

    Add the container of questions

--- src/App.jsx
+++ src/App.jsx
@@ -6,51 +6,32 @@ import { useState } from 'react';
 
 function App() {
 
-  const [questionNum, setQuestionNum] = useState(0);
-
-  const data = {
-    questions: [
-      {
-        title: 'Please select your industry.',
-        description: '',
-        options: [
-          { title: 'Retail', description: '' },
-          { title: 'Hospitality', description: '' },
-          { title: 'Healthcare', description: '' },
-          { title: 'Education', description: '' }
-        ],
-        showMore: ''
-      }
-    ]
-  }
-
   return (
-    <> 
+    <>
       <BasePage />
       <Feature />
-    <
-    <div className='header'>
-      <h1 className='header__title'>{data.questions[questionNum].title}</h1>
-      <p className='header__description'>{data.questions[questionNum].description}</p>
-    </div>
-    <div className='main'>
-      <div className='main-container'>
-        <p className='main-container__title'>{data.questions[questionNum].options[0].title}</p>
-        <p className='main-container__description'>{data.questions[questionNum].options[0].description}</p>
+      {/* <div className='header'>
+        <h1 className='header__title'>{data.questions[questionNum].title}</h1>
+        <p className='header__description'>{data.questions[questionNum].description}</p>
       </div>
-      <div className='main-container'>
-        <p className='main-container__title'>{data.questions[questionNum].options[1].title}</p>
-        <p className='main-container__description'>{data.questions[questionNum].options[1].description}</p>
-      </div>
-      <div className='main-container'>
-        <p className='main-container__title'>{data.questions[questionNum].options[2].title}</p>
-        <p className='main-container__description'>{data.questions[questionNum].options[2].description}</p>
-      </div>
-      <div className='main-container'>
-        <p className='main-container__title'>{data.questions[questionNum].options[3].title}</p>
-        <p className='main-container__description'>{data.questions[questionNum].options[3].description}</p>
-      </div>
-    </div>
+      <div className='main'>
+        <div className='main-container'>
+          <p className='main-container__title'>{data.questions[questionNum].options[0].title}</p>
+          <p className='main-container__description'>{data.questions[questionNum].options[0].description}</p>
+        </div>
+        <div className='main-container'>
+          <p className='main-container__title'>{data.questions[questionNum].options[1].title}</p>
+          <p className='main-container__description'>{data.questions[questionNum].options[1].description}</p>
+        </div>
+        <div className='main-container'>
+          <p className='main-container__title'>{data.questions[questionNum].options[2].title}</p>
+          <p className='main-container__description'>{data.questions[questionNum].options[2].description}</p>
+        </div>
+        <div className='main-container'>
+          <p className='main-container__title'>{data.questions[questionNum].options[3].title}</p>
+          <p className='main-container__description'>{data.questions[questionNum].options[3].description}</p>
+        </div>
+      </div> */}
     </>
   )
 }
